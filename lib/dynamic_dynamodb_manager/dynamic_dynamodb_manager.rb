76a47fa929c80c2a3ce0222d84c1df70e1a68e30
nickveenhof <nick.veenhof@gmail.com>
2014-10-15 14:53:47 +0200

    Write the config file using an easy rake task to a path of your choice

--- lib/dynamic_dynamodb_manager/dynamic_dynamodb_manager.rb
+++ lib/dynamic_dynamodb_manager/dynamic_dynamodb_manager.rb
@@ -1,3 +1,28 @@
+require 'open-uri'
+require 'aws-sdk'
+require 'json'
+require 'dynamic_dynamodb_manager/dynamic_dynamodb_manager'
+require 'erb'
+require 'dotenv'
+require 'pp'
+require 'open-uri'
+
+ENV['RACK_ENV'] ||= 'development'
+puts "Loading with environment #{ENV['RACK_ENV']}"
+Dotenv.load("#{ENV['RACK_ENV']}.env")
+
+if ENV['BUGSNAG_APIKEY']
+  require 'bugsnag'
+  Bugsnag.configure do |config|
+    config.api_key = ENV['BUGSNAG_APIKEY']
+    config.use_ssl = true
+    config.notify_release_stages = ['production']
+    config.project_root = '/search'
+    config.app_version = ENV['VERSION']
+    config.release_stage = ENV['RACK_ENV']
+  end
+end
+
 class DynamicDynamoDBManager
 
   attr_reader :dynamo_client
