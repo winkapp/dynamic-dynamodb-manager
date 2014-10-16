45af3d0dfebb4038da6b0a9d85b1867bcf4c7251
nickveenhof <nick.veenhof@gmail.com>
2014-10-16 12:18:57 +0200

    Adding rotation handling

--- lib/dynamic_dynamodb_manager.rb
+++ lib/dynamic_dynamodb_manager.rb
@@ -5,10 +5,11 @@ require 'erb'
 require 'dotenv'
 require 'pp'
 require 'open-uri'
+require 'date'
 
 ENV['RACK_ENV'] ||= 'development'
 puts "Loading with environment #{ENV['RACK_ENV']}"
-Dotenv.load("#{ENV['RACK_ENV']}.env")
+Dotenv.load("../#{ENV['RACK_ENV']}.env")
 
 if ENV['BUGSNAG_APIKEY']
   require 'bugsnag'
@@ -32,16 +33,16 @@ class DynamicDynamoDBManager
     # Setup default configs using dotenv libraries
     ENV['DYNAMODB_ENDPOINT'] ||= "localhost"
     ENV['DYNAMODB_PORT'] ||= 4567
-    ENV['AWS_ACCESS_KEY'] ||= "00000"
-    ENV['AWS_SECRET_ACCESS_KEY'] ||= "00000"
-    ENV['DYNAMODB_API_VERSION'] ||= "2012-08-10"
+    ENV['AWS_ACCESS_KEY'] ||= '00000'.to_i
+    ENV['AWS_SECRET_ACCESS_KEY'] ||= '00000'.to_i
+    ENV['DYNAMODB_API_VERSION'] ||= '2012-08-10'
     ENV['DYNAMODB_USE_SSL'] ||= false
     ENV['API_TABLE_RESOURCE'] ||= 'http://testing.com/v1/system/tables'
 
     default_configs = { :dynamo_db_endpoint => ENV['DYNAMODB_ENDPOINT'],
                    :dynamo_db_port     => ENV['DYNAMODB_PORT'].to_i,
-                   :access_key_id      => ENV['AWS_ACCESS_KEY'],
-                   :secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'],
+                   :access_key_id      => ENV['AWS_ACCESS_KEY'].to_s,
+                   :secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'].to_s,
                    :api_version        => ENV['DYNAMODB_API_VERSION'],
                    :use_ssl            => ENV['DYNAMODB_USE_SSL'] == 1 }
 
@@ -61,23 +62,92 @@ class DynamicDynamoDBManager
     dynamo_client.list_tables[:table_names]
   end
 
-  def get_api_tables()
-   api_tables
+  def get_all_tables()
+    tables = Array.new
+
+    api_tables.each do | table|
+      rotation_scheme = table['RotationScheme']
+      purge_rotation = table['PurgeRotation'].to_i
+      table_name = table['TableName']
+      environment = ENV['RACK_ENV']
+
+      case rotation_scheme
+        when "daily"
+          # syntax
+          # stack.tablename.20041011
+          i = 0
+          start_date = Date.today + 1
+          days = purge_rotation + 1
+          while i < days  do
+            table_prefix = start_date-i
+            i=i+1
+            # Create a deep copy by taking the variables and copy them into the new object
+            # @see http://ruby.about.com/od/advancedruby/a/deepcopy.htm
+            temp_table = Marshal.load(Marshal.dump(table))
+            temp_table['TableName'] = "#{environment}.#{table_name}."+table_prefix.strftime('%Y%m%d')
+            tables << temp_table
+          end
+        when "weekly"
+          # syntax
+          # stack.tablename.20041011
+          i = 0
+          days = purge_rotation * 7 + 1
+          start_date = date_of_next('monday')
+          while i < days  do
+            table_prefix = start_date-i
+            i=i+7
+            # Create a deep copy by taking the variables and copy them into the new object
+            # @see http://ruby.about.com/od/advancedruby/a/deepcopy.htm
+            temp_table = Marshal.load(Marshal.dump(table))
+            temp_table['TableName'] = "#{environment}.#{table_name}."+table_prefix.strftime('%Y%m%d')
+            tables << temp_table
+          end
+        when "monthly"
+          i = 0
+          # Add one more month so we have a buffer when we purge
+          months = purge_rotation+1
+          # Create a dateTime object with x+1 month in the past so we can
+          # start counting up from there
+          dt = Time.new().to_datetime. << months
+          while i < months  do
+            # Setting our real date with 2 months in the future as offset
+            table_prefix = dt >> i+2
+            i=i+1
+            # Create a deep copy by taking the variables and copy them into the new object
+            # @see http://ruby.about.com/od/advancedruby/a/deepcopy.htm
+            temp_table = Marshal.load(Marshal.dump(table))
+            # Forced to 01 as we always want the first of the month
+            temp_table['TableName'] = "#{environment}.#{table_name}."+table_prefix.strftime('%Y%m%d')
+            tables << temp_table
+          end
+        else
+          raise "RotationScheme #{rotation_scheme} is not supported"
+      end
+    end
+    tables
   end
 
+  def date_of_next(day)
+    date  = Date.parse(day)
+    delta = date > Date.today ? 0 : 7
+    date + delta
+  end
+
+
   def create_dynamodb_tables()
-    api_tables.each{ | table |
+    tables = get_all_tables
+    tables.each do | table |
 
       attribute_definitions = Array.new
-      table['Properties']['AttributeDefinitions'].each { |ad|
+      table['Properties']['AttributeDefinitions'].each do |ad|
         attribute = { attribute_name: ad['AttributeName'], attribute_type: ad['AttributeType'] }
         attribute_definitions << attribute
-      }
+      end
       key_schema = Array.new
-      table['Properties']['KeySchema'].each { |ad|
+      table['Properties']['KeySchema'].each do |ad|
         attribute = { attribute_name: ad['AttributeName'], key_type: ad['KeyType'] }
         key_schema << attribute
-      }
+      end
 
       read_capacity_units = table['Properties']['ProvisionedThroughput']['ReadCapacityUnits'].to_i
       write_capacity_units = table['Properties']['ProvisionedThroughput']['WriteCapacityUnits'].to_i
@@ -88,8 +158,36 @@ class DynamicDynamoDBManager
                            provisioned_throughput: { read_capacity_units: read_capacity_units, write_capacity_units: write_capacity_units },
                            table_name: table_name }
 
-      dynamo_client.create_table(new_table_params)
-    }
+      # Do not create tables that already exist
+      unless collections.include?(table_name)
+        puts "Creating #{table_name}."
+        dynamo_client.create_table(new_table_params)
+      end
+    end
+  end
+
+  def cleanup_tables()
+    all_tables = get_all_tables
+
+    # Get all our expected tables
+    expected_tables = Array.new
+    all_tables.each do | table |
+      expected_tables << table['TableName']
+    end
+
+    # Get all existing tables
+    existing_tables = collections
+
+    # Make the diff between those.
+    remaining_tables = existing_tables - expected_tables
+
+    # Remove all tables that are remaining
+    remaining_tables.each do | table_name |
+      if collections.include?(table_name)
+        puts "Deleting #{table_name}."
+        dynamo_client.delete_table({table_name: table_name })
+      end
+    end
   end
 
   def write_dynamic_dynamodb_config(path = '/tmp/dynamic-dynamodb.conf')
--- spec/stories/get_api_table_spec.rb
+++ spec/stories/get_api_table_spec.rb
@@ -9,7 +9,7 @@ describe DynamicDynamoDBManager do
   end
 
   it 'returns a list of tables' do
-    tables = @manager.get_api_tables
+    tables = @manager.get_all_tables
     expect(tables[0]['TableName']).to eq ("Sessions")
     expect(tables[0]['RotationScheme']).to eq ("daily")
     expect(tables[0]['PurgeRotation']).to eq ("4")
