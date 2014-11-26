e517c855e2e7e5a9956426b944e7ffc518d85bf1
nickveenhof <nick.veenhof@gmail.com>
2014-11-26 17:55:10 +0100

    Use rack environment so travis doesnt fail

--- spec/stories/create_table_from_api_spec.rb
+++ spec/stories/create_table_from_api_spec.rb
@@ -9,32 +9,32 @@ describe DynamicDynamoDBManager do
       @manager.create_dynamodb_tables
     end
     # No rotation has no timestamp
-    expect(@manager.collections.include?("development.norotation")).to eq(true)
+    expect(@manager.collections.include?(ENV['RACK_ENV']+".norotation")).to eq(true)
 
     # Daily with rotate 2 means 1 day ahead and 2 behind
-    expect(@manager.collections.include?("development.daily-purge2.20141225")).to eq(true)
-    expect(@manager.collections.include?("development.daily-purge2.20141224")).to eq(true)
-    expect(@manager.collections.include?("development.daily-purge2.20141223")).to eq(true)
+    expect(@manager.collections.include?(ENV['RACK_ENV']+".daily-purge2.20141225")).to eq(true)
+    expect(@manager.collections.include?(ENV['RACK_ENV']+".daily-purge2.20141224")).to eq(true)
+    expect(@manager.collections.include?(ENV['RACK_ENV']+".daily-purge2.20141223")).to eq(true)
 
     # Weekly with rotate 2 means 1 week ahead and 2 behind. Starting on the next monday for the one
     # in the future.
-    expect(@manager.collections.include?("development.weekly-purge2.20141229")).to eq(true)
-    expect(@manager.collections.include?("development.weekly-purge2.20141222")).to eq(true)
-    expect(@manager.collections.include?("development.weekly-purge2.20141215")).to eq(true)
+    expect(@manager.collections.include?(ENV['RACK_ENV']+".weekly-purge2.20141229")).to eq(true)
+    expect(@manager.collections.include?(ENV['RACK_ENV']+".weekly-purge2.20141222")).to eq(true)
+    expect(@manager.collections.include?(ENV['RACK_ENV']+".weekly-purge2.20141215")).to eq(true)
 
     # Monthly with rotate 4 means 1 month ahead and 4 behind. Starting on the first day of the month.
-    expect(@manager.collections.include?("development.monthly-purge4.20150124")).to eq(true)
-    expect(@manager.collections.include?("development.monthly-purge4.20140924")).to eq(true)
-    expect(@manager.collections.include?("development.monthly-purge4.20141024")).to eq(true)
-    expect(@manager.collections.include?("development.monthly-purge4.20141124")).to eq(true)
-    expect(@manager.collections.include?("development.monthly-purge4.20141224")).to eq(true)
+    expect(@manager.collections.include?(ENV['RACK_ENV']+".monthly-purge4.20150124")).to eq(true)
+    expect(@manager.collections.include?(ENV['RACK_ENV']+".monthly-purge4.20140924")).to eq(true)
+    expect(@manager.collections.include?(ENV['RACK_ENV']+".monthly-purge4.20141024")).to eq(true)
+    expect(@manager.collections.include?(ENV['RACK_ENV']+".monthly-purge4.20141124")).to eq(true)
+    expect(@manager.collections.include?(ENV['RACK_ENV']+".monthly-purge4.20141224")).to eq(true)
 
     # daily with rotate 4 means 1 day ahead and 4 behind. The nopurge means they will not get deleted.
-    expect(@manager.collections.include?("development.daily-nopurge.20141225")).to eq(true)
-    expect(@manager.collections.include?("development.daily-nopurge.20141224")).to eq(true)
-    expect(@manager.collections.include?("development.daily-nopurge.20141223")).to eq(true)
-    expect(@manager.collections.include?("development.daily-nopurge.20141222")).to eq(true)
-    expect(@manager.collections.include?("development.daily-nopurge.20141221")).to eq(true)
+    expect(@manager.collections.include?(ENV['RACK_ENV']+".daily-nopurge.20141225")).to eq(true)
+    expect(@manager.collections.include?(ENV['RACK_ENV']+".daily-nopurge.20141224")).to eq(true)
+    expect(@manager.collections.include?(ENV['RACK_ENV']+".daily-nopurge.20141223")).to eq(true)
+    expect(@manager.collections.include?(ENV['RACK_ENV']+".daily-nopurge.20141222")).to eq(true)
+    expect(@manager.collections.include?(ENV['RACK_ENV']+".daily-nopurge.20141221")).to eq(true)
   end
 
   it 'returns a list of created tables' do
@@ -57,64 +57,64 @@ describe DynamicDynamoDBManager do
     # Verify we created the tables
     puts "Checking if all tables of that date were created"
     # No rotation has no timestamp
-    expect(@manager.collections.include?("development.norotation")).to eq(true)
+    expect(@manager.collections.include?(ENV['RACK_ENV']+".norotation")).to eq(true)
 
     # Daily with rotate 2 means 1 day ahead and 2 behind
-    expect(@manager.collections.include?("development.daily-purge2.20140902")).to eq(true)
-    expect(@manager.collections.include?("development.daily-purge2.20140901")).to eq(true)
-    expect(@manager.collections.include?("development.daily-purge2.20140831")).to eq(true)
+    expect(@manager.collections.include?(ENV['RACK_ENV']+".daily-purge2.20140902")).to eq(true)
+    expect(@manager.collections.include?(ENV['RACK_ENV']+".daily-purge2.20140901")).to eq(true)
+    expect(@manager.collections.include?(ENV['RACK_ENV']+".daily-purge2.20140831")).to eq(true)
 
     # Weekly with rotate 2 means 1 week ahead and 2 behind. Starting on the next monday for the one
     # in the future.
-    expect(@manager.collections.include?("development.weekly-purge2.20140908")).to eq(true)
-    expect(@manager.collections.include?("development.weekly-purge2.20140901")).to eq(true)
-    expect(@manager.collections.include?("development.weekly-purge2.20140825")).to eq(true)
+    expect(@manager.collections.include?(ENV['RACK_ENV']+".weekly-purge2.20140908")).to eq(true)
+    expect(@manager.collections.include?(ENV['RACK_ENV']+".weekly-purge2.20140901")).to eq(true)
+    expect(@manager.collections.include?(ENV['RACK_ENV']+".weekly-purge2.20140825")).to eq(true)
 
     # Monthly with rotate 4 means 1 month ahead and 4 behind. Starting on the first day of the month.
-    expect(@manager.collections.include?("development.monthly-purge4.20140601")).to eq(true)
-    expect(@manager.collections.include?("development.monthly-purge4.20140701")).to eq(true)
-    expect(@manager.collections.include?("development.monthly-purge4.20140801")).to eq(true)
-    expect(@manager.collections.include?("development.monthly-purge4.20140901")).to eq(true)
-    expect(@manager.collections.include?("development.monthly-purge4.20141001")).to eq(true)
+    expect(@manager.collections.include?(ENV['RACK_ENV']+".monthly-purge4.20140601")).to eq(true)
+    expect(@manager.collections.include?(ENV['RACK_ENV']+".monthly-purge4.20140701")).to eq(true)
+    expect(@manager.collections.include?(ENV['RACK_ENV']+".monthly-purge4.20140801")).to eq(true)
+    expect(@manager.collections.include?(ENV['RACK_ENV']+".monthly-purge4.20140901")).to eq(true)
+    expect(@manager.collections.include?(ENV['RACK_ENV']+".monthly-purge4.20141001")).to eq(true)
 
     # daily with rotate 4 means 1 day ahead and 4 behind. The nopurge means they will not get deleted.
-    expect(@manager.collections.include?("development.daily-nopurge.20140902")).to eq(true)
-    expect(@manager.collections.include?("development.daily-nopurge.20140901")).to eq(true)
-    expect(@manager.collections.include?("development.daily-nopurge.20140831")).to eq(true)
-    expect(@manager.collections.include?("development.daily-nopurge.20140830")).to eq(true)
-    expect(@manager.collections.include?("development.daily-nopurge.20140829")).to eq(true)
+    expect(@manager.collections.include?(ENV['RACK_ENV']+".daily-nopurge.20140902")).to eq(true)
+    expect(@manager.collections.include?(ENV['RACK_ENV']+".daily-nopurge.20140901")).to eq(true)
+    expect(@manager.collections.include?(ENV['RACK_ENV']+".daily-nopurge.20140831")).to eq(true)
+    expect(@manager.collections.include?(ENV['RACK_ENV']+".daily-nopurge.20140830")).to eq(true)
+    expect(@manager.collections.include?(ENV['RACK_ENV']+".daily-nopurge.20140829")).to eq(true)
 
     Timecop.freeze Date.new(2014,12,24) do
       # Now we cleanup the tables so that only the tables of today remain
       puts "Cleaning up unnecessary tables"
       @manager.cleanup_tables
       # No rotation has no timestamp
-      expect(@manager.collections.include?("development.norotation")).to eq(true)
+      expect(@manager.collections.include?(ENV['RACK_ENV']+".norotation")).to eq(true)
 
       # Daily with rotate 2 means 1 day ahead and 2 behind
-      expect(@manager.collections.include?("development.daily-purge2.20140902")).to eq(false)
-      expect(@manager.collections.include?("development.daily-purge2.20140901")).to eq(false)
-      expect(@manager.collections.include?("development.daily-purge2.20140831")).to eq(false)
+      expect(@manager.collections.include?(ENV['RACK_ENV']+".daily-purge2.20140902")).to eq(false)
+      expect(@manager.collections.include?(ENV['RACK_ENV']+".daily-purge2.20140901")).to eq(false)
+      expect(@manager.collections.include?(ENV['RACK_ENV']+".daily-purge2.20140831")).to eq(false)
 
       # Weekly with rotate 2 means 1 week ahead and 2 behind. Starting on the next monday for the one
       # in the future.
-      expect(@manager.collections.include?("development.weekly-purge2.20140908")).to eq(false)
-      expect(@manager.collections.include?("development.weekly-purge2.20140901")).to eq(false)
-      expect(@manager.collections.include?("development.weekly-purge2.20140825")).to eq(false)
+      expect(@manager.collections.include?(ENV['RACK_ENV']+".weekly-purge2.20140908")).to eq(false)
+      expect(@manager.collections.include?(ENV['RACK_ENV']+".weekly-purge2.20140901")).to eq(false)
+      expect(@manager.collections.include?(ENV['RACK_ENV']+".weekly-purge2.20140825")).to eq(false)
 
       # Monthly with rotate 4 means 1 month ahead and 4 behind. Starting on the first day of the month.
-      expect(@manager.collections.include?("development.monthly-purge4.20140601")).to eq(false)
-      expect(@manager.collections.include?("development.monthly-purge4.20140701")).to eq(false)
-      expect(@manager.collections.include?("development.monthly-purge4.20140801")).to eq(false)
-      expect(@manager.collections.include?("development.monthly-purge4.20140901")).to eq(false)
-      expect(@manager.collections.include?("development.monthly-purge4.20141001")).to eq(false)
+      expect(@manager.collections.include?(ENV['RACK_ENV']+".monthly-purge4.20140601")).to eq(false)
+      expect(@manager.collections.include?(ENV['RACK_ENV']+".monthly-purge4.20140701")).to eq(false)
+      expect(@manager.collections.include?(ENV['RACK_ENV']+".monthly-purge4.20140801")).to eq(false)
+      expect(@manager.collections.include?(ENV['RACK_ENV']+".monthly-purge4.20140901")).to eq(false)
+      expect(@manager.collections.include?(ENV['RACK_ENV']+".monthly-purge4.20141001")).to eq(false)
 
       # daily with rotate 4 means 1 day ahead and 4 behind. The nopurge means they will not get deleted.
-      expect(@manager.collections.include?("development.daily-nopurge.20140902")).to eq(true)
-      expect(@manager.collections.include?("development.daily-nopurge.20140901")).to eq(true)
-      expect(@manager.collections.include?("development.daily-nopurge.20140831")).to eq(true)
-      expect(@manager.collections.include?("development.daily-nopurge.20140830")).to eq(true)
-      expect(@manager.collections.include?("development.daily-nopurge.20140829")).to eq(true)
+      expect(@manager.collections.include?(ENV['RACK_ENV']+".daily-nopurge.20140902")).to eq(true)
+      expect(@manager.collections.include?(ENV['RACK_ENV']+".daily-nopurge.20140901")).to eq(true)
+      expect(@manager.collections.include?(ENV['RACK_ENV']+".daily-nopurge.20140831")).to eq(true)
+      expect(@manager.collections.include?(ENV['RACK_ENV']+".daily-nopurge.20140830")).to eq(true)
+      expect(@manager.collections.include?(ENV['RACK_ENV']+".daily-nopurge.20140829")).to eq(true)
     end
   end
 
